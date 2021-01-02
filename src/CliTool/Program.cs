b5383f41f94f507d6183d6aaec5455813ab5c60e
David Cabrera <dacabrer@microsoft.com>
2021-01-02 15:46:44 -0800

    Setup repository + first projects + style cops + PR validation actions (#1)
    * template gitignore
    * setup projects
    * add pr validation action
    * minor changes in pr validation action
    * fix actions
    * working on actions and build

---
+++ src/CliTool/Program.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="Program.cs" company="Simoorg">
+// Licensed under the MIT license. See LICENSE file in the samples root for full license information.
+// </copyright>
+
+namespace Simoorg.FillUpDoc
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+        }
+    }
+}
---
+++ src/Test/UnitTests/SampleTest.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="SampleTest.cs" company="Simoorg">
+// Licensed under the MIT license. See LICENSE file in the samples root for full license information.
+// </copyright>
+
+namespace Prime.UnitTests.Services
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class SampleTest
+    {
+        [TestMethod]
+        public void SampleTest1()
+        {
+            Assert.IsFalse(false);
+        }
+    }
+}
