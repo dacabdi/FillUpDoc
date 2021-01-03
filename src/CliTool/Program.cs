37f96164756631e8b869080c12e1503c4ee244dd
David Cabrera <dacabrer@microsoft.com>
2021-01-02 19:24:52 -0800

    setup old poc

--- src/CliTool/Program.cs
+++ src/CliTool/Program.cs
@@ -4,10 +4,65 @@
 
 namespace Simoorg.FillUpDoc
 {
-    internal class Program
+    using System;
+    using System.IO;
+    using DocumentFormat.OpenXml.Packaging;
+    using DocumentFormat.OpenXml.Wordprocessing;
+    using Newtonsoft.Json.Linq;
+    using NLog;
+
+    public static class Program
     {
-        private static void Main()
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        public static int Main(string[] args)
         {
+            Logger.Info("~~~~~~~~~~~~~~ Running simmorg session! ~~~~~~~~~~~~~~");
+
+            if (args.Length <= 1)
+            {
+                Logger.Fatal("Did not provide enough arguments.");
+                return 1;
+            }
+
+            string templatePath = args[0];
+            string jsonPath = args[1];
+            string outputPath = args[2];
+
+            File.Copy(templatePath, outputPath, true);
+
+            Logger.Info($"Using template file: {templatePath}");
+            Logger.Info($"Using JSON file: {jsonPath}");
+            Logger.Info($"Output to: {outputPath}");
+
+            try
+            {
+                // open json file
+                using WordprocessingDocument wordTemplate = WordprocessingDocument.Open(outputPath, true);
+                Logger.Info("Parsing JSON");
+                JObject json = JObject.Parse(File.ReadAllText(jsonPath));
+
+                Logger.Info("Parsing Template Body");
+                Body body = wordTemplate.MainDocumentPart.Document.Body;
+
+                Logger.Info("Generating new Template Structure");
+                TemplateTraversor generator = new StructureGenerator(body, json);
+                generator.Traverse();
+
+                Logger.Info("Merging JSON content into Template");
+                TemplateTraversor merger = new TemplateJsonMerger(body, json);
+                merger.Traverse();
+            }
+            catch (Exception ex)
+            {
+                Logger.Fatal(ex, "A fatal error ocurred.");
+                Logger.Fatal(ex.StackTrace);
+                return 1;
+            }
+
+            Logger.Info("Session ended!");
+
+            return 0;
         }
     }
 }
---
+++ src/Lib/StructureGenerator.cs
@@ -0,0 +1,98 @@
+﻿// <copyright file="StructureGenerator.cs" company="Simoorg">
+// Licensed under the MIT license. See LICENSE file in the samples root for full license information.
+// </copyright>
+
+namespace Simoorg.FillUpDoc
+{
+    using System.Linq;
+    using DocumentFormat.OpenXml;
+    using DocumentFormat.OpenXml.Wordprocessing;
+    using Newtonsoft.Json.Linq;
+    using NLog;
+
+    public class StructureGenerator : TemplateTraversor
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        public StructureGenerator(OpenXmlElement xmlRoot, JObject jsonRoot)
+            : base(xmlRoot, jsonRoot)
+        {
+        }
+
+        protected override JToken Matcher(OpenXmlElement node, JToken json, string tag)
+        {
+            return this.GetJTokenFromKey((JContainer)json, tag);
+        }
+
+        protected override void VisitNode(OpenXmlElement node, JToken json, string tag, string strippedTag, int level)
+        {
+            Logger.Trace($"StructureGenerator.VisitNode:[json.Type={json.Type}]");
+            switch (json.Type)
+            {
+                case JTokenType.Array:
+
+                    Logger.Info($"Tag '{tag}' matched array '{json.Path}'");
+
+                    OpenXmlElement parent = node.Parent;
+                    int count = json.Count();
+
+                    // if empty array, delete current entry
+                    if (count == 0)
+                    {
+                        Logger.Warn($"Array matching '{tag}' is empty, removing content control");
+                        parent.RemoveChild(node);
+                    }
+                    else
+                    {
+                        Logger.Info($"Retagging template's original content control ");
+                        Logger.Info($"Cloning and appending '{tag}' {count} times");
+
+                        node = parent.RemoveChild(node);
+
+                        // parentNode = parent.RemoveChild(node);
+
+                        // otherwise, repeat as needed, and attach to parent
+                        for (int i = 0; i < count; ++i)
+                        {
+                            OpenXmlElement clone = node.CloneNode(true); // OpenXmlElement clone = parentNode.CloneNode(true);
+
+                            this.DoTraverseAllChildren(clone, json[i], level + 1);
+                            this.SetSdtElementTagValue((SdtElement)clone, $"{tag}{i}");
+                            parent.AppendChild(clone);
+                        }
+                    }
+
+                    break;
+
+                case JTokenType.Object:
+
+                    Logger.Info($"Tag '{tag}' matched object '{json.Path}'");
+
+                    if (json.Count() == 0)
+                    {
+                        Logger.Warn($"JSON Object is empty, removing content control");
+                        node.Parent.RemoveChild(node);
+                    }
+                    else
+                    {
+                        this.DoTraverseAllChildren(node, json, level);
+                    }
+
+                    break;
+
+                case JTokenType.Null:
+
+                    Logger.Warn($"Tag '{tag}' matched null object '{json.Path}', removing content control");
+                    node.Parent.RemoveChild(node);
+
+                    break;
+
+                default:
+
+                    Logger.Debug($"Tag '{tag}' matched '{json.Path}', non structural content control, leave as is.");
+
+                    break;
+            }
+        }
+    }
+}
